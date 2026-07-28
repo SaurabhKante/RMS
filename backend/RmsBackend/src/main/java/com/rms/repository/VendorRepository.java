61ac9451649cad872ef4ce136d94fb9b99965356
VedantKshirsagar <vedantkshirsagar07@gmail.com>
2026-07-28 14:46:16 +0530

    Expense Feature Added

--- backend/RmsBackend/src/main/java/com/rms/repository/VendorRepository.java
+++ backend/RmsBackend/src/main/java/com/rms/repository/VendorRepository.java
@@ -2,11 +2,15 @@ package com.rms.repository;
 
 import com.rms.entity.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface VendorRepository extends JpaRepository<Vendor, Integer> {
+
+    Optional<Vendor> findByVendorIdAndIsActiveTrue(Integer vendorId);
 
     Optional<Vendor> findByVendorName(String vendorName);
 
